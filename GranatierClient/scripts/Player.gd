8d1b88b77dcdc610ca9cc3815df996babec164e6
Orchi1904 <alexej1@web.de>
2022-05-23 16:09:03 +0200

    Added player stats on client side

--- GranatierClient/scripts/Player.gd
+++ GranatierClient/scripts/Player.gd
@@ -3,9 +3,21 @@ extends KinematicBody2D
 var speed = 100
 #var screen_size
 
+var can_kick = false
+var can_throw = false
+var has_shield = false
+var layable_bombs = 1
+var bomb_blast_range = 1
+var is_scatty = false
+var is_restrained = false
+var has_mirror = false
+var has_teleport = false
+var has_bad_powerup = false
+
 #func _ready():
 	#screen_size = get_viewport_rect().size
 var nextPositions = []
+var powerups = []
 
 func set_animation(player_no):
 	$AnimatedSprite.animation = "player" + str(player_no)
