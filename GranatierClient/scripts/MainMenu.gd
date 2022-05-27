3de3fc4797aaba792698fafcb4458c24bb2b2233
Airem97 <Frank2949@gmx.de>
2022-05-27 10:01:48 +0200

    Adjusted main menu and map selection

--- GranatierClient/scripts/MainMenu.gd
+++ GranatierClient/scripts/MainMenu.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
 # Paths to the different scenes. Can be added through the scene inspector
-export var startGameScene = "res://scenes/World.tscn"
-export var highscoreScene = "res://scenes/Highscore.tscn"
+export var createSessionScene = "res://scenes/MapSelection.tscn"
+export var joinSessionScene = "res://scenes/Session.tscn"
 export var settingsScene = "res://scenes/Settings.tscn"
 
 # Called when the node enters the scene tree for the first time.
@@ -25,11 +25,11 @@ func load_scene(var path):
 
 # Loads the game
 func _on_StartGame_pressed():
-	load_scene(startGameScene)
+	load_scene(createSessionScene)
 
 # Loads the highscore scene
 func _on_Highscore_pressed():
-	load_scene(highscoreScene)
+	load_scene(joinSessionScene)
 
 # Loads the settings scene
 func _on_Settings_pressed():
--- GranatierClient/scripts/MapSelection.gd
+++ GranatierClient/scripts/MapSelection.gd
@@ -1,15 +1,31 @@
 extends Node2D
 
+const PATH = "res://resources/maps"
+
 export var mainMenu = "res://scenes/MainMenu.tscn"
-export var arena = "clanbomber_Arena"
-export var broken_Heart = "clanbomber_Broken_Heart"
-export var three_of_three = "threeofthree"
+export var world = "res://scenes/World.tscn"
 
 signal map(map_name)
 
+var selectedItem
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var label
+	var dir = Directory.new()
+	dir.open(PATH)
+	dir.list_dir_begin()
+	
+	var file_name = dir.get_next()
+	
+	while file_name != "":
+		if !dir.current_is_dir():
+			file_name.erase(file_name.length() - 4, 4)
+			$MapList.add_item(file_name)
+		file_name = dir.get_next()
+		
+	$MapList.select(0)
+	selectedItem = $MapList.get_item_text(0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -25,23 +41,15 @@ func load_scene(var path):
 			ERR_CANT_CREATE:
 				print("Error: cant create scene")
 
-# Emit signal to the world scene with the map name
-func load_map(map_name):
-	emit_signal("map", map_name)
-	self.visible = false
-
 # Return to the main menu
 func _on_Return_pressed():
 	load_scene(mainMenu)
 
-# Load the map "Arena"
-func _on_Arena_pressed():
-	load_map(arena)
 
-# Load the map "Broken Heart"
-func _on_BrokenHeart_pressed():
-	load_map(broken_Heart)
+func _on_MapList_item_selected(index):
+	selectedItem = $MapList.get_item_text(index)
 
-# Load the map "Three of Three"
-func _on_ThreeOfThree_pressed():
-	load_map(three_of_three)
+# Emit signal with the map name, then load the world scene
+func _on_CreateSession_pressed():
+	emit_signal("map", selectedItem)
+	load_scene(world)
