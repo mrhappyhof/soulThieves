a8f37d3e5df37a3eb59222d4f4ec870a24b5456c
Airem97 <Frank2949@gmx.de>
2022-05-29 14:05:36 +0200

    Improved HUD

--- GranatierClient/scripts/HUD.gd
+++ GranatierClient/scripts/HUD.gd
@@ -7,6 +7,7 @@ signal lose()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$PlayerList.set_icon_scale(0.2)
+	$PlayerList.allow_reselect = false
 	pass # Replace with function body.
 
 
@@ -14,6 +15,7 @@ func _ready():
 #func _process(delta):
 	#pass
 
+# Timer function
 func countdown():
 	if time >= 0:
 		var minutes = time / 60
@@ -25,12 +27,19 @@ func countdown():
 		emit_signal("lose")
 		#queue_free()
 
+# Display players in the HUD
 func hud_display_player():
 	$PlayerList.clear()
 	var i = 1
 	
+	# Get all players from node "Players" and place them in the PlayerList
 	for child in get_node("../Players").get_children():
 		var sprite = child.get_node("AnimatedSprite")
 		var frame = sprite.get_sprite_frames().get_frame(sprite.get_animation(), sprite.get_frame())
+		
 		$PlayerList.add_item("Player " + str(i), frame)
 		i += 1
+	
+	# Disable Tooltip for all items
+	for index in $PlayerList.get_item_count():
+		$PlayerList.set_item_tooltip_enabled(index, false)
