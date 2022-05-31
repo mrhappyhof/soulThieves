2aae5aa346fff3a059fd0f652b849b608ec4192b
Orchi1904 <alexej1@web.de>
2022-05-31 21:04:33 +0200

    Added ability to create a lobby in my UI

--- GranatierClient/scripts/CreateNewLobby.gd
+++ GranatierClient/scripts/CreateNewLobby.gd
@@ -1,6 +1,6 @@
 extends CanvasLayer
 
-signal map(map_name)
+signal session(session_name, map_name)
 
 export var gameSelectionScene = "res://scenes/GameSelection.tscn"
 
@@ -17,13 +17,30 @@ onready var chooseMapBtn = get_node(chooseMapBtn_path)
 onready var lobbyNameField = get_node("MenuBackground/MarginContainer/VBoxContainer/LobbyNameField")
 
 var selectedMapId
-
-var maps = ["Arena", "Broken Heart", "Three of Three", "T", "T", "T", "T", "T", "T", "T", "T"]
+var selectedItem
+const PATH = "res://resources/maps"
+export var world = "res://scenes/World.tscn"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#var label
+	var dir = Directory.new()
+	dir.open(PATH)
+	dir.list_dir_begin()
+	
+	var file_name = dir.get_next()
+	
+	# Load every map and cut of the file extension
+	while file_name != "":
+		if !dir.current_is_dir():
+			file_name.erase(file_name.length() - 4, 4)
+			itemList.add_item(file_name)
+		file_name = dir.get_next()
+		
+	itemList.select(0)
+	selectedItem = itemList.get_item_text(0)
+	
 	lobbyNameField.grab_focus()
-	add_map_items()
 
 
 # Checks if the scene at the given path exists and loads it
@@ -35,11 +52,6 @@ func load_scene(var path):
 				print("Error: cant open " + path)
 			ERR_CANT_CREATE:
 				print("Error: cant create scene")
-
-#adds items (maps) to the itemList
-func add_map_items():
-	for map in maps:
-		itemList.add_item(map)
 
 func _on_Back_pressed():
 	load_scene(gameSelectionScene)
@@ -65,4 +77,7 @@ func _on_ItemList_item_selected(index):
 
 
 func _on_CreateBtn_pressed():
-	emit_signal("map", maps[selectedMapId])
+	emit_signal("session", lobbyNameField.text, selectedItem)
+	Server.create_session(lobbyNameField.text, selectedItem)
+	load_scene(world)
+	
